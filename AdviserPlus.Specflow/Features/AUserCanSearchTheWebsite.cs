c3847e1d09f301adcacc88cb238fa0dc69fc35c4
Michelle Riley <michelle.riley85@gmail.com>
2020-09-18 22:47:05 +0100

    Initial commit

---
+++ AdviserPlus.Specflow/Features/AUserCanSearchTheWebsite.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AdviserPlus.Specflow.Features
+{
+    [Binding]
+    public sealed class AUserCanSearchTheWebsite
+    {
+        private IWebDriver _driver = new ChromeDriver();
+
+        [Given(@"I am on the homepage")]
+        public void GivenIAmOnTheHomepage()
+        {
+            _driver.Navigate().GoToUrl("https://adviserplus.com/");
+            _driver.Dispose();
+        }
+    }
+}
---
+++ AdviserPlus.Specflow/Features/AUserCanSearchTheWebsite.feature
@@ -0,0 +1,7 @@
+﻿Feature: AUserCanSearchTheWebsite
+	As a user
+	I want to be able to search the website
+	So that I can easily find the desired content
+
+Scenario: A search term with no results
+	Given I am on the homepage
